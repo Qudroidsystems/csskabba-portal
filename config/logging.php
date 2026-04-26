818b986b50dfa1f8dd92715d55d8dad68de06248
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-04-26 12:55:15 +0100

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+        'pdf' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/pdf.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
 
     ],
 
