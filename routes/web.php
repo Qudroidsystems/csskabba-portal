902be9dbd62070b4fb49a1f9e133f6cdcb85861f
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-05-07 18:37:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -715,10 +715,6 @@ Route::post('/students/bulk-remove-from-term', [StudentController::class, 'bulkR
     });
 
 
-   <?php
-
-use App\Http\Controllers\Reports\FinancialReportController;
-
 // ============================================
 // FINANCIAL REPORTS ROUTES
 // ============================================
