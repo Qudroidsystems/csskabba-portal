aaa1b4dd1a5a546dcdf53cfc7bd766d4f797b15f
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-06-05 12:54:38 +0100

    Update class_results_pdf.blade.php

--- resources/views/studentreports/class_results_pdf.blade.php
+++ resources/views/studentreports/class_results_pdf.blade.php
@@ -39,7 +39,7 @@
 
         /* SCHOOL NAME HEADER */
         .school-name-header {
-            width: 95%;
+            width: 97%;
             background: #111827;
             color: white;
             padding: 7px 10px 5px 10px;
