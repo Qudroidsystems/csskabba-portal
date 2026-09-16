0f0a8a4e4e4694180ffd1694955f8c4aa12ca92f
Eliab <35658748+eliabsiji@users.noreply.github.com>
2026-09-16 13:56:18 +0100

    working

--- app/Http/Controllers/SubjectClassController.php
+++ app/Http/Controllers/SubjectClassController.php
@@ -11,10 +11,11 @@ use App\Models\Subjectclass;
 use App\Models\SubjectRegistrationStatus;
 use App\Models\SubjectTeacher;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Yajra\DataTables\Facades\DataTables;
 
